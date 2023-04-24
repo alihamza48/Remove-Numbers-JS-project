88cb64bf9a1aad6b617be94b8c5f9f34c66a68a6
Syed Ali Hamza Naqvi <129213252+alihamza48@users.noreply.github.com>
2023-04-25 03:40:04 +0500

    Add files via upload

---
+++ J.js
@@ -0,0 +1,144 @@
+function one()
+{
+
+
+
+let a = document.querySelector('#o1')
+if (a.style.display !== 'none')
+{
+    a.style.display = "none"
+}
+let b = document.querySelector('#o2')
+if (b.style.display !== 'none')
+{
+    b.style.display = "none"
+}
+let c = document.querySelector('#o3')
+if (c.style.display === 'none')
+{
+    c.style.display = "block"
+}
+let d = document.querySelector('#o4')
+if (d.style.display === 'none')
+{
+    d.style.display = "block"
+}
+let e = document.querySelector('#o5')
+if (e.style.display === 'none')
+{
+    e.style.display = "block"
+}
+let f = document.querySelector('#o6')
+if (f.style.display === 'none')
+{
+    f.style.display = "block"
+}
+}
+
+function two()
+{
+
+let a = document.querySelector('#o3')
+if (a.style.display !== 'none')
+{
+    a.style.display = "none"
+}
+let b = document.querySelector('#o4')
+if (b.style.display !== 'none')
+{
+    b.style.display = "none"
+}
+let c = document.querySelector('#o1')
+if (c.style.display === 'none')
+{
+    c.style.display = "block"
+}
+let d = document.querySelector('#o2')
+if (d.style.display === 'none')
+{
+    d.style.display = "block"
+}
+let e = document.querySelector('#o5')
+if (e.style.display === 'none')
+{
+    e.style.display = "block"
+}
+let f = document.querySelector('#o6')
+if (f.style.display === 'none')
+{
+    f.style.display = "block"
+}
+
+}
+
+function three()
+{
+
+let a = document.querySelector('#o5')
+if (a.style.display !== 'none')
+{
+    a.style.display = "none"
+}
+let b = document.querySelector('#o6')
+if (b.style.display !== 'none')
+{
+    b.style.display = "none"
+}
+let c = document.querySelector('#o1')
+if (c.style.display === 'none')
+{
+    c.style.display = "block"
+}
+let d = document.querySelector('#o2')
+if (d.style.display === 'none')
+{
+    d.style.display = "block"
+}
+let e = document.querySelector('#o3')
+if (e.style.display === 'none')
+{
+    e.style.display = "block"
+}
+let f = document.querySelector('#o4')
+if (f.style.display === 'none')
+{
+    f.style.display = "block"
+}
+
+}
+
+function four()
+{
+
+let a = document.querySelector('#o5')
+if (a.style.display === 'none')
+{
+    a.style.display = "block"
+}
+let b = document.querySelector('#o6')
+if (b.style.display === 'none')
+{
+    b.style.display = "block"
+}
+let c = document.querySelector('#o1')
+if (c.style.display === 'none')
+{
+    c.style.display = "block"
+}
+let d = document.querySelector('#o2')
+if (d.style.display === 'none')
+{
+    d.style.display = "block"
+}
+let e = document.querySelector('#o3')
+if (e.style.display === 'none')
+{
+    e.style.display = "block"
+}
+let f = document.querySelector('#o4')
+if (f.style.display === 'none')
+{
+    f.style.display = "block"
+}
+
+}
